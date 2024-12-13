d00dd9f1dc7563a26af5b0395d9019421fa8ca4d
Nonthawit Doungsodsri <nonthawit.kub@gmail.com>
2024-12-14 00:40:53 +0700

    [+] update package name

--- apps/playground/scripts/deploy_web_to_staging.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Run sudo chmod 755 before use.
-
-echo "🌏 Deploy Flutter Web to Staging..."
-peanut --branch gh-pages-staging --release --web-renderer html --extra-args "--dart-define=flavor=prod"
-git push origin gh-pages-staging
--- apps/playground/scripts/regenerate_configs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Run sudo chmod 755 restart_pub_get.sh before use.
-
-echo "[Generate Configs]"
-flutter clean
-flutter pub get
-flutter pub run flutter_flavorizr
-flutter pub run flutter_native_splash:create --flavors dev,uat,prod,widget
-flutter pub run flutter_launcher_icons
