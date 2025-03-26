3dcf4ad1ced7b7e35f1c6c50418bd1afbbd74fbd
Nuttapong Phisitbutr <nuttapong.p@nextzy.com>
2025-03-26 15:13:02 +0700

    Add checkbox to header [AppTable]

--- apps/widgetbook/lib/src/widgets/tables/table_overview_case.dart
+++ apps/widgetbook/lib/src/widgets/tables/table_overview_case.dart
@@ -31,6 +31,7 @@ class OverviewTableWidgetCase extends WidgetbookScrollableUseCase {
                     ),
                     rowsPerPage: 4,
                     filteredColumn: 0,
+                    hasCheckbox: false,
                   ),
                 ),
               ],
@@ -46,12 +47,14 @@ class AppTable extends StatefulWidget {
     required this.dataTableSource,
     this.rowsPerPage = 0,
     this.filteredColumn = 0,
+    this.hasCheckbox = false,
   });
 
   final List<String> headerNames;
   final AppDataTableSource dataTableSource;
   final int rowsPerPage;
   final int filteredColumn;
+  final bool hasCheckbox;
 
   @override
   State<AppTable> createState() => _AppTableState();
@@ -100,7 +103,6 @@ class _AppTableState extends State<AppTable> {
             ),
           ),
         ),
-        // Space.gap20,
         ...[
           Row(
             children: [
@@ -110,9 +112,14 @@ class _AppTableState extends State<AppTable> {
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        AppText(
-                          widget.headerNames[i],
-                          textAlign: TextAlign.center,
+                        if (widget.hasCheckbox) AppCheckbox(label: ''),
+                        Flexible(
+                          child: Text(
+                            widget.headerNames[i],
+                            textAlign: TextAlign.center,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                          ),
                         ),
                         if (i == _currentSortColumn)
                           Icon(
@@ -143,11 +150,11 @@ class _AppTableState extends State<AppTable> {
           child: ListView.separated(
             itemCount: _visibleCellContainers.length,
             itemBuilder: (context, index) {
-              final rowWidgets = _visibleCellContainers[index];
+              final cellContainers = _visibleCellContainers[index];
 
               return Row(
-                children: List.generate(rowWidgets.cells.length, (index) {
-                  return Expanded(child: rowWidgets.cells[index].widget);
+                children: List.generate(cellContainers.cells.length, (index) {
+                  return Expanded(child: cellContainers.cells[index].widget);
                 }),
               );
             },
@@ -473,261 +480,261 @@ DateTime getRandomTime() {
 
 //----------------------------------------------------------
 
-// class MyPaginatedDataTable extends StatefulWidget {
-//   const MyPaginatedDataTable({super.key});
-//
-//   @override
-//   State<MyPaginatedDataTable> createState() => _MyPaginatedDataTableState();
-// }
-//
-// class _MyPaginatedDataTableState extends State<MyPaginatedDataTable> {
-//   bool _isSortAscending = true;
-//   int? _currentSortColumnIndex;
-//
-//   final persons = MyPerson.getPersons();
-//   List<MyPerson> filteredPersons = [];
-//
-//   final _controller = TextEditingController();
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//
-//     filteredPersons = persons.toList();
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return PaginatedDataTable2(
-//       header: Padding(
-//         padding: const EdgeInsets.only(top: 20),
-//         child: AppTextField(
-//           // controller: _controller,
-//           placeholderText: 'Search..',
-//           onTextChange: (t) {
-//             setState(() {
-//               print('t: $t');
-//               filteredPersons = persons
-//                   .where((e) => e.name.toLowerCase().contains(t.toLowerCase()))
-//                   .toList();
-//               print('f: ${filteredPersons.length}');
-//             });
-//           },
-//         ),
-//       ),
-//       showFirstLastButtons: true,
-//       showCheckboxColumn: true,
-//       headingCheckboxTheme: CheckboxThemeData(
-//         shape: RoundedRectangleBorder(
-//           borderRadius: BorderRadius.circular(4),
-//         ),
-//       ),
-//       datarowCheckboxTheme: CheckboxThemeData(
-//         shape: RoundedRectangleBorder(
-//           borderRadius: BorderRadius.circular(4),
-//         ),
-//       ),
-//       rowsPerPage: 5,
-//       wrapInCard: false,
-//       // fit: FlexFit.loose,
-//       fit: FlexFit.tight,
-//       renderEmptyRowsInTheEnd: false,
-//       minWidth: 1000,
-//       // headingRowHeight: 100,
-//       // dataRowHeight: 120,
-//       // columnSpacing: 200,
-//       onPageChanged: (value) {},
-//       hidePaginator: false,
-//       sortArrowAlwaysVisible: true,
-//       sortAscending: _isSortAscending,
-//       sortColumnIndex: _currentSortColumnIndex,
-//       // onRowsPerPageChanged: (no) {},
-//       columns: [
-//         // DataColumn2(
-//         //   // size: ColumnSize.L,
-//         //   label: Padding(
-//         //     padding: const EdgeInsets.only(top: 10),
-//         //     child: TextFormField(
-//         //       controller: _controller,
-//         //       autofocus: false,
-//         //       decoration: InputDecoration(
-//         //         hintText: 'Search..',
-//         //         fillColor: Colors.white,
-//         //         focusedBorder: OutlineInputBorder(
-//         //           borderRadius: BorderRadius.circular(10),
-//         //           borderSide: BorderSide(
-//         //             color: Colors.blue,
-//         //           ),
-//         //         ),
-//         //         enabledBorder: OutlineInputBorder(
-//         //           borderRadius: BorderRadius.circular(10),
-//         //           borderSide: BorderSide(
-//         //             color: Colors.grey,
-//         //           ),
-//         //         ),
-//         //       ),
-//         //       onChanged: (t) {
-//         //         setState(() {
-//         //           print('t: $t');
-//         //           filteredPersons = persons
-//         //               .where(
-//         //                   (e) => e.name.toLowerCase().contains(t.toLowerCase()))
-//         //               .toList();
-//         //           print('f: ${filteredPersons.length}');
-//         //         });
-//         //       },
-//         //     ),
-//         //   ),
-//         // ),
-//         // DataColumn2(
-//         //   // size: ColumnSize.L,
-//         //   label: Padding(
-//         //     padding: const EdgeInsets.only(top: 10),
-//         //     child: AppTextField(
-//         //       controller: _controller,
-//         //       placeholderText: 'Search..',
-//         //       onTextChange: (t) {
-//         //         setState(() {
-//         //           print('t: $t');
-//         //           filteredPersons = persons
-//         //               .where(
-//         //                   (e) => e.name.toLowerCase().contains(t.toLowerCase()))
-//         //               .toList();
-//         //           print('f: ${filteredPersons.length}');
-//         //         });
-//         //       },
-//         //     ),
-//         //   ),
-//         // ),
-//         DataColumn2(
-//           label: AppText('Name'),
-//           onSort: (columnIndex, isSortAscending) {
-//             setState(() {
-//               _currentSortColumnIndex = 0;
-//
-//               if (isSortAscending) {
-//                 filteredPersons.sort((a, b) => a.name.compareTo(b.name));
-//               } else {
-//                 filteredPersons.sort((a, b) => b.name.compareTo(a.name));
-//               }
-//
-//               _isSortAscending = !_isSortAscending;
-//             });
-//           },
-//         ),
-//         DataColumn2(
-//           // size: ColumnSize.S,
-//           label: AppText('Rating'),
-//           onSort: (columnIndex, isSortAscending) {
-//             _currentSortColumnIndex = 1;
-//             setState(() {
-//               if (isSortAscending) {
-//                 filteredPersons.sort((a, b) => a.rating.compareTo(b.rating));
-//               } else {
-//                 filteredPersons.sort((a, b) => b.rating.compareTo(a.rating));
-//               }
-//
-//               _isSortAscending = !_isSortAscending;
-//             });
-//           },
-//         ),
-//         DataColumn2(
-//           // size: ColumnSize.S,
-//           label: AppText('Result'),
-//           onSort: (columnIndex, isSortAscending) {
-//             setState(() {
-//               _currentSortColumnIndex = 2;
-//
-//               if (isSortAscending) {
-//                 filteredPersons.sort((a, b) => a.result.compareTo(b.result));
-//               } else {
-//                 filteredPersons.sort((a, b) => b.result.compareTo(a.result));
-//               }
-//
-//               _isSortAscending = !_isSortAscending;
-//             });
-//           },
-//         ),
-//         DataColumn2(
-//           // size: ColumnSize.S,
-//           label: AppText('Last contact'),
-//           onSort: (columnIndex, isSortAscending) {
-//             setState(() {
-//               _currentSortColumnIndex = 3;
-//
-//               if (isSortAscending) {
-//                 filteredPersons
-//                     .sort((a, b) => a.lastContact.compareTo(b.lastContact));
-//               } else {
-//                 filteredPersons
-//                     .sort((a, b) => b.lastContact.compareTo(a.lastContact));
-//               }
-//
-//               _isSortAscending = !_isSortAscending;
-//             });
-//           },
-//         ),
-//         DataColumn2(
-//           label: AppText('More info'),
-//         ),
-//       ],
-//       source: MyDataSource(filteredPersons),
-//     );
-//   }
-// }
-//
-// class MyDataSource extends DataTableSource {
-//   final List<MyPerson> _data;
-//
-//   MyDataSource(this._data);
-//
-//   @override
-//   DataRow getRow(int index) {
-//     final item = _data[index];
-//
-//     return DataRow2(
-//       // selected: true,
-//       cells: [
-//         DataCell(
-//           Row(
-//             children: [
-//               AppCircleAvatar(
-//                 style: WidgetStyle.subtle,
-//                 size: WidgetSize.md,
-//                 path: Assets.mock.avatarSquared1.keyName,
-//               ),
-//               Space.gap8,
-//               AppText(item.name),
-//             ],
-//           ),
-//           // AppText(item.name),
-//         ),
-//         DataCell(
-//           AppText(item.rating.toString()),
-//         ),
-//         DataCell(
-//           AppText(item.result),
-//         ),
-//         DataCell(
-//           AppText(DateFormat('yyyy-MM-dd').format(item.lastContact)),
-//         ),
-//         DataCell(
-//           AppOutlineButton(
-//             text: 'See more ->',
-//             size: WidgetSize.sm,
-//           ),
-//         ),
-//       ],
-//       onSelectChanged: (e) => {},
-//     );
-//   }
-//
-//   @override
-//   int get rowCount => _data.length;
-//
-//   @override
-//   int get selectedRowCount => 0;
-//
-//   @override
-//   bool get isRowCountApproximate => false;
-// }
+class MyPaginatedDataTable extends StatefulWidget {
+  const MyPaginatedDataTable({super.key});
+
+  @override
+  State<MyPaginatedDataTable> createState() => _MyPaginatedDataTableState();
+}
+
+class _MyPaginatedDataTableState extends State<MyPaginatedDataTable> {
+  bool _isSortAscending = true;
+  int? _currentSortColumnIndex;
+
+  final persons = MyPerson.getPersons();
+  List<MyPerson> filteredPersons = [];
+
+  final _controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    filteredPersons = persons.toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PaginatedDataTable2(
+      header: Padding(
+        padding: const EdgeInsets.only(top: 20),
+        child: AppTextField(
+          // controller: _controller,
+          placeholderText: 'Search..',
+          onTextChange: (t) {
+            setState(() {
+              print('t: $t');
+              filteredPersons = persons
+                  .where((e) => e.name.toLowerCase().contains(t.toLowerCase()))
+                  .toList();
+              print('f: ${filteredPersons.length}');
+            });
+          },
+        ),
+      ),
+      showFirstLastButtons: true,
+      showCheckboxColumn: true,
+      headingCheckboxTheme: CheckboxThemeData(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4),
+        ),
+      ),
+      datarowCheckboxTheme: CheckboxThemeData(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4),
+        ),
+      ),
+      rowsPerPage: 5,
+      wrapInCard: false,
+      // fit: FlexFit.loose,
+      fit: FlexFit.tight,
+      renderEmptyRowsInTheEnd: false,
+      minWidth: 1000,
+      // headingRowHeight: 100,
+      // dataRowHeight: 120,
+      // columnSpacing: 200,
+      onPageChanged: (value) {},
+      hidePaginator: false,
+      sortArrowAlwaysVisible: true,
+      sortAscending: _isSortAscending,
+      sortColumnIndex: _currentSortColumnIndex,
+      // onRowsPerPageChanged: (no) {},
+      columns: [
+        // DataColumn2(
+        //   // size: ColumnSize.L,
+        //   label: Padding(
+        //     padding: const EdgeInsets.only(top: 10),
+        //     child: TextFormField(
+        //       controller: _controller,
+        //       autofocus: false,
+        //       decoration: InputDecoration(
+        //         hintText: 'Search..',
+        //         fillColor: Colors.white,
+        //         focusedBorder: OutlineInputBorder(
+        //           borderRadius: BorderRadius.circular(10),
+        //           borderSide: BorderSide(
+        //             color: Colors.blue,
+        //           ),
+        //         ),
+        //         enabledBorder: OutlineInputBorder(
+        //           borderRadius: BorderRadius.circular(10),
+        //           borderSide: BorderSide(
+        //             color: Colors.grey,
+        //           ),
+        //         ),
+        //       ),
+        //       onChanged: (t) {
+        //         setState(() {
+        //           print('t: $t');
+        //           filteredPersons = persons
+        //               .where(
+        //                   (e) => e.name.toLowerCase().contains(t.toLowerCase()))
+        //               .toList();
+        //           print('f: ${filteredPersons.length}');
+        //         });
+        //       },
+        //     ),
+        //   ),
+        // ),
+        // DataColumn2(
+        //   // size: ColumnSize.L,
+        //   label: Padding(
+        //     padding: const EdgeInsets.only(top: 10),
+        //     child: AppTextField(
+        //       controller: _controller,
+        //       placeholderText: 'Search..',
+        //       onTextChange: (t) {
+        //         setState(() {
+        //           print('t: $t');
+        //           filteredPersons = persons
+        //               .where(
+        //                   (e) => e.name.toLowerCase().contains(t.toLowerCase()))
+        //               .toList();
+        //           print('f: ${filteredPersons.length}');
+        //         });
+        //       },
+        //     ),
+        //   ),
+        // ),
+        DataColumn2(
+          label: AppText('Name'),
+          onSort: (columnIndex, isSortAscending) {
+            setState(() {
+              _currentSortColumnIndex = 0;
+
+              if (isSortAscending) {
+                filteredPersons.sort((a, b) => a.name.compareTo(b.name));
+              } else {
+                filteredPersons.sort((a, b) => b.name.compareTo(a.name));
+              }
+
+              _isSortAscending = !_isSortAscending;
+            });
+          },
+        ),
+        DataColumn2(
+          // size: ColumnSize.S,
+          label: AppText('Rating'),
+          onSort: (columnIndex, isSortAscending) {
+            _currentSortColumnIndex = 1;
+            setState(() {
+              if (isSortAscending) {
+                filteredPersons.sort((a, b) => a.rating.compareTo(b.rating));
+              } else {
+                filteredPersons.sort((a, b) => b.rating.compareTo(a.rating));
+              }
+
+              _isSortAscending = !_isSortAscending;
+            });
+          },
+        ),
+        DataColumn2(
+          // size: ColumnSize.S,
+          label: AppText('Result'),
+          onSort: (columnIndex, isSortAscending) {
+            setState(() {
+              _currentSortColumnIndex = 2;
+
+              if (isSortAscending) {
+                filteredPersons.sort((a, b) => a.result.compareTo(b.result));
+              } else {
+                filteredPersons.sort((a, b) => b.result.compareTo(a.result));
+              }
+
+              _isSortAscending = !_isSortAscending;
+            });
+          },
+        ),
+        DataColumn2(
+          // size: ColumnSize.S,
+          label: AppText('Last contact'),
+          onSort: (columnIndex, isSortAscending) {
+            setState(() {
+              _currentSortColumnIndex = 3;
+
+              if (isSortAscending) {
+                filteredPersons
+                    .sort((a, b) => a.lastContact.compareTo(b.lastContact));
+              } else {
+                filteredPersons
+                    .sort((a, b) => b.lastContact.compareTo(a.lastContact));
+              }
+
+              _isSortAscending = !_isSortAscending;
+            });
+          },
+        ),
+        DataColumn2(
+          label: AppText('More info'),
+        ),
+      ],
+      source: MyDataSource(filteredPersons),
+    );
+  }
+}
+
+class MyDataSource extends DataTableSource {
+  final List<MyPerson> _data;
+
+  MyDataSource(this._data);
+
+  @override
+  DataRow getRow(int index) {
+    final item = _data[index];
+
+    return DataRow2(
+      // selected: true,
+      cells: [
+        DataCell(
+          Row(
+            children: [
+              AppCircleAvatar(
+                style: WidgetStyle.subtle,
+                size: WidgetSize.md,
+                path: Assets.mock.avatarSquared1.keyName,
+              ),
+              Space.gap8,
+              AppText(item.name),
+            ],
+          ),
+          // AppText(item.name),
+        ),
+        DataCell(
+          AppText(item.rating.toString()),
+        ),
+        DataCell(
+          AppText(item.result),
+        ),
+        DataCell(
+          AppText(DateFormat('yyyy-MM-dd').format(item.lastContact)),
+        ),
+        DataCell(
+          AppOutlineButton(
+            text: 'See more ->',
+            size: WidgetSize.sm,
+          ),
+        ),
+      ],
+      onSelectChanged: (e) => {},
+    );
+  }
+
+  @override
+  int get rowCount => _data.length;
+
+  @override
+  int get selectedRowCount => 0;
+
+  @override
+  bool get isRowCountApproximate => false;
+}
