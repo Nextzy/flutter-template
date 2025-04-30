070d84bb6119777a4594cb3c6ee9666c131ef96d
Thanaphon Namjittong <thanaphon.namj@bumail.net>
2025-04-30 16:08:20 +0700

    Update Link widgetbook feedback

--- packages/design_system/lib/widgets/displays/link.dart
+++ packages/design_system/lib/widgets/displays/link.dart
@@ -5,13 +5,19 @@ class AppLink extends AppStatelessWidget {
     super.key,
     required this.text,
     required this.url,
+    this.color,
     this.fontSize = 14,
+    this.fontStyle,
+    this.underline = true,
     this.disabled = false,
   });
 
   final String text;
   final String url;
+  final Color? color;
   final double fontSize;
+  final FontStyle? fontStyle;
+  final bool underline;
   final bool disabled;
 
   @override
@@ -22,12 +28,14 @@ class AppLink extends AppStatelessWidget {
             disabled ? null : AppUrlScheme.launchExternalApp(Uri.parse(url)),
         padding: const EdgeInsets.only(bottom: 4),
         decoration: BoxDecoration(
-            border: Border(
-                bottom: BorderSide(
-          color: disabled
-              ? context.theme.color.textTertiary
-              : context.theme.color.brandPrimary,
-        ))),
+            border: underline
+                ? Border(
+                    bottom: BorderSide(
+                    color: disabled
+                        ? context.theme.color.textTertiary
+                        : color ?? context.theme.color.brandPrimary,
+                  ))
+                : null),
         child: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
@@ -35,16 +43,17 @@ class AppLink extends AppStatelessWidget {
                 style: TextStyle(
                   fontSize: fontSize,
                   fontWeight: FontWeight.w400,
+                  fontStyle: fontStyle,
                   color: disabled
                       ? context.theme.color.textTertiary
-                      : context.theme.color.brandPrimary,
+                      : color ?? context.theme.color.brandPrimary,
                 )),
             Gap(4),
             Assets.icon.arrowSquareOutRegular.svgIcon(
                 colorFilter: ColorFilter.mode(
                     disabled
                         ? context.theme.color.textTertiary
-                        : context.theme.color.brandPrimary,
+                        : color ?? context.theme.color.brandPrimary,
                     BlendMode.srcIn),
                 size: fontSize)
           ],
